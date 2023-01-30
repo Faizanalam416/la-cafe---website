032fd0219dc30b84f55b2449cdb018981241882f
Faizan <fa09042003@gmail.com>
2023-01-30 19:53:41 +0530

    navbar change

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ let subMenu = document.getElementById("subMenu");
 function toggleMenu() {
     subMenu.classList.toggle("open-menu");
 }
+
+
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled',$(this).scrollTop()>100);
+});
